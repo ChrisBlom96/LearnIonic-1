372adadfbbb85bd490f9edebb64663f77aa20af8
ChrisBlom96 <chrisb@intelliacc.com>
2023-07-24 13:38:32 +0200

    Push all the things made today Login Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,26 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+
+  {
+    path: 'Search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+
+  {
+    path: 'Profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  
+
 ];
 
 @NgModule({
